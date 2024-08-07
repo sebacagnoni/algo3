455fdeb2a370a6cb9d9e244fcc463cfce619757b
sebadev97 <sebacagnoni.proy@gmail.com>
2024-08-07 00:06:24 -0300

    Add some changes

---
+++ tp1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+// execute as ./main < main.in
+int main()
+{
+    int c;
+    for (int i = 0; i < 7; i++)
+    {
+        cin >> c;
+        cout << c;
+    }
+
+    return 0;
+}
